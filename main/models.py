242339d64dc2329b7e2deae4db5e2f97cf14b8c2
ErstevanAlmeida <erstevan2306@gmail.com>
2023-09-25 13:44:27 +0700

    menyimpan tugas 4 sementara

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     collection = models.CharField(max_length=255, default='-')
     type = models.CharField(max_length=255, default="-")
--- main/views.py
+++ main/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from main.models import Product
-from main.forms import NewCollectionForm
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from main.forms import NewCollectionForm
+from main.models import Product
+import datetime
 
+@login_required(login_url='/login/')
 def show_main(request):
-    collection = Product.objects.all()
+    collection = Product.objects.filter(user=request.user)
     collection_count = len(collection)
 
     context = {
@@ -17,6 +22,7 @@ def show_main(request):
         'class': 'PBP - E',
         'products': collection,
         'collection_count': collection_count,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -25,7 +31,9 @@ def create_collection(request):
     form = NewCollectionForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        collection = form.save(commit=False)
+        collection.user = request.user
+        collection.save()
         return HttpResponseRedirect(reverse('main:show_main'))
     
     context = {'form' : form}
@@ -46,3 +54,36 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been succesfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return redirect('main:login')
